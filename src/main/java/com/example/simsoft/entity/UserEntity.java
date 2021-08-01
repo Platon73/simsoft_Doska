3dcb65a2eb767d7e08ab3c304a49ff639902a97b
Platon73 <platonova_0@list.ru>
2021-08-01 15:24:25 +0400

    Запросы к базе данных для Todo запросы выполняются. вытягиваются данные по юзерам и по Todo

--- src/main/java/com/example/simsoft/entity/UserEntity.java
+++ src/main/java/com/example/simsoft/entity/UserEntity.java
@@ -18,6 +18,14 @@ public class UserEntity {
     public UserEntity() {
     }
 
+    public List<TodoEntity> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<TodoEntity> todos) {
+        this.todos = todos;
+    }
+
     public Long getId() {
         return id;
     }
