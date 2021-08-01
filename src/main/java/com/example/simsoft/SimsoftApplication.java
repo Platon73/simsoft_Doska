e2143568fc388727cbd30fe41d2e94e7d89b192e
Platon73 <platonova_0@list.ru>
2021-08-01 12:05:10 +0400

    Пустой начальный проект

---
+++ src/main/java/com/example/simsoft/SimsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.simsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimsoftApplication.class, args);
+    }
+
+}
