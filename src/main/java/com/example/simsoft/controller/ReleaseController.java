bba65d318782dbda89d04dd26844ec99476e448e
Platon73 <platonova_0@list.ru>
2021-08-05 10:19:00 +0400

    добавил ReleaseController и метод creatRelesae все запускается но пока не отрабатывает - в базе не создана таблица для релиза

---
+++ src/main/java/com/example/simsoft/controller/ReleaseController.java
@@ -0,0 +1,27 @@
+package com.example.simsoft.controller;
+
+import com.example.simsoft.entity.ReleaseEntity;
+import com.example.simsoft.entity.TodoEntity;
+import com.example.simsoft.service.ReleaseService;
+import com.example.simsoft.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/release")
+public class ReleaseController {
+
+    @Autowired
+    private ReleaseService releaseService;
+
+    @PostMapping
+    public ResponseEntity creatRelease(@RequestBody ReleaseEntity releaseEntity,
+                                    @RequestParam Long userId){
+        try {
+            return ResponseEntity.ok(releaseService.creatRelease(releaseEntity, userId));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+}
